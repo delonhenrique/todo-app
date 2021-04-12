07235833ef3eafcd0f022afb63beff587f10ecf6
delonhenrique <delon.henrique@gmail.com>
2021-04-12 00:24:54 -0300

    Config API

--- backend/src/api/todo/todo.js
+++ backend/src/api/todo/todo.js
@@ -1,4 +1,4 @@
-const restful = require("node-restful");
+const restful = require('node-restful');
 const mongoose = restful.mongoose;
 const todoSchema = new mongoose.Schema({
     descripition: { type: String, requied: true },
